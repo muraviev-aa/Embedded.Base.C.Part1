ab23cdd474b2e326c4eea436b36924ffb1a7d623
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-04-06 11:58:56 +0300

    Initial commit

---
+++ hw1/2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Let\'s\n");
+    printf("%5s\n", "go");
+    printf("%12s", "to walk");
+    return 0;
+}
---
+++ hw1/3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%4s\n", "*");
+    printf("%5s\n", "***");
+    printf("%6s\n", "*****");
+    printf("%7s\n", "*******");
+    printf("%6s\n", "HHOHH");
+    printf("%6s\n", "ZZZZZ");
+    return 0;
+}
