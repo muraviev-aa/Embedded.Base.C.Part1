1fe12585ebb266c7f0551e659705b21c8ded52d8
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-04-09 20:57:25 +0300

    Added changes to 4_1.c

--- hw2/4_1.c
+++ hw2/4_1.c
@@ -2,15 +2,17 @@
  * Доказать тождество А->В =!A||B
  */
 #include <stdio.h>
+#include <windows.h>
 
 int main()
 {
+    SetConsoleOutputCP(CP_UTF8);
     printf("%s\t%s\t!A\t!A||B\tA->B\n", "A", "B");
     printf("0\t0\t1\t1\t1\n");
     printf("0\t1\t1\t1\t1\n");
     printf("1\t0\t0\t0\t0\n");
     printf("1\t1\t0\t1\t1\n");
-    printf("The identity is proven.\n");
+    printf("Тождество доказано.");
     return 0;
 }
 
