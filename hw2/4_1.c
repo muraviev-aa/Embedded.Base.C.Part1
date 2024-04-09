3805552f291bcc49f5169bc32426bb0c01942d2c
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-04-09 20:15:34 +0300

    Added 4_1.c

---
+++ hw2/4_1.c
@@ -0,0 +1,15 @@
+/*
+ * Доказать тождество А->В =!A||B
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("%s\t%s\t!A\t!A||B\tA->B\n", "A", "B");
+    printf("0\t0\t1\t1\t1\n");
+    printf("0\t1\t1\t1\t1\n");
+    printf("1\t0\t0\t0\t0\n");
+    printf("1\t1\t0\t1\t1\n");
+    return 0;
+}
+
