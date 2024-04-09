ff2185f84d641903b8b84aa36f33a7179ff00658
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-04-09 20:50:29 +0300

    Added changes to 4_1.c

--- hw2/4_1.c
+++ hw2/4_1.c
@@ -10,7 +10,7 @@ int main()
     printf("0\t1\t1\t1\t1\n");
     printf("1\t0\t0\t0\t0\n");
     printf("1\t1\t0\t1\t1\n");
-    printf("The identity is proven.");
+    printf("The identity is proven.\n");
     return 0;
 }
 
