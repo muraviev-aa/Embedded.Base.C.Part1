34d0f1e1588cd10785b99d0ecc6ce74d206b1a79
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-04-12 22:26:27 +0300

    Added changes to 1.c in hw3

--- hw3/1.c
+++ hw3/1.c
@@ -6,7 +6,7 @@ int main()
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
     int a, b, c;
-    printf("Введите три целых чичла через пробел\n");
+    printf("Введите три целых числа через пробел\n");
     scanf("%d%d%d", &a, &b, &c);
     printf("%d+%d+%d=%d", a, b, c, a+b+c);
     return 0;
