9386aec96d3de3ef3e05df267cb09a73f0557263
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-04-20 00:19:25 +0300

    Added changes 1.c to hw5

--- hw5/1.c
+++ hw5/1.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void)
-{
+int main(void) {
     int a;
     scanf("%d", &a);
     if (a > 0 && a <= 100)
     {
-        for (int i=1; i <= a; i++)
-            printf("%d %d %d\n", i, (int)pow(i, 2), (int)pow(i, 3));
+        for (int i = 1; i <= a; i++)
+            printf("%d %d %d\n", i, (int) pow(i, 2), (int) pow(i, 3));
     }
     else
         printf("Incorrect number entered");
