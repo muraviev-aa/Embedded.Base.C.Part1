de06c9c469b92504cd9dd3b2dc5bfa6115fe589a
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-04-20 09:00:47 +0300

    Added 3.c to hw5

---
+++ hw5/3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a, b, sum = 0;
+    scanf("%d%d", &a, &b);
+    for (int i = a; i <= b; i++)
+        sum += (int) pow(i, 2);
+    printf("%d", sum);
+    return 0;
+}
