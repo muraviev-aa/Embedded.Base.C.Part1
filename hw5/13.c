296b57542ce57c5f7b74aaabd1bbd11993d63f04
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-04-21 11:18:17 +0300

    Added 13.c to hw5

---
+++ hw5/13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, even = 0, odd = 0;
+    scanf("%d", &a);
+    while(a > 0)
+    {
+        if(a%10%2 == 0)
+            even++;
+        else
+            odd++;
+        a /= 10;
+    }
+    printf("%d %d", even, odd);
+    return 0;
+}
