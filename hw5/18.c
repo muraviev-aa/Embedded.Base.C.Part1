8d6e583dd7b5ee3f5f67435d4827500758733b4f
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-04-22 17:40:38 +0300

    Added 18.c to hw5

---
+++ hw5/18.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i, f1 = 1, f2 = 1, m;
+    scanf("%d", &n);
+    if (n == 1)
+        printf("1");
+    else
+    if (n != 0)
+    {
+        printf("1 1 ");
+        for (i = 2; i < n; ++i)
+        {
+            m = f1 + f2;
+            f1 = f2;
+            f2 = m;
+            printf("%d ", f2);
+        }
+    }
+    return 0;
+}
