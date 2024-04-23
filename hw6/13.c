f5fe9531dd607e7e32b2dd47a539ba007e59409e
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-04-23 17:47:53 +0300

    Added 13.c to hw6

---
+++ hw6/13.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+const double PI = 3.1416;
+
+double series_sum(double x);
+
+int main(void)
+{
+    float x;
+    scanf("%f", &x);
+    printf("%.3f", series_sum(x));
+    return 0;
+}
+
+double series_sum(double x)
+{
+    double result = 1.0, s = 1.0, fact = 1.0, pow = 1.0;
+    x = x * (PI / 180);
+    for (int i = 1; i < 5; i++)
+    {
+        s = s * -1;
+        fact = fact * (2 * i - 1) * (2 * i);
+        pow = pow * x * x;
+        result = result + s * pow / fact;
+    }
+    return result;
+}
