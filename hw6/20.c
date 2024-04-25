d3ff33dbc6e4a4328e54326f68f456b19eb68d0e
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-04-25 18:52:35 +0300

    Added 20.c to hw6

---
+++ hw6/20.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int ch, a = 0;
+    do
+    {
+        ch = getchar();
+        if (ch == '(')
+            a++;
+        else if (ch == ')' && --a < 0)
+            break;
+    }
+    while (ch != '.');
+
+    if (a == 0)
+        printf("YES");
+    else
+        printf("NO");
+    return 0;
+}
