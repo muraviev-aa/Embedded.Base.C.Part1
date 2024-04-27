f1d0cbedde1485cf4799b47ea904de1b31e74aca
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-04-27 19:06:29 +0300

    Added 8.c to hw7

---
+++ hw7/8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void func1(int a, int b)
+{
+    if (a <= b)
+    {
+        printf("%d ", a);
+        func1(a + 1, b);
+    }
+}
+
+void func2(int a, int b)
+{
+    if (a >= b)
+    {
+        printf("%d ", a);
+        func2(a - 1, b);
+
+    }
+}
+
+int main(void)
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    if (a < b)
+        func1(a, b);
+    else if (a > b)
+        func2(a, b);
+    else
+        printf("%d", a);
+
+}
