e9f2aac944a6d923e05705bc442fa4b72e3f36db
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-05-04 09:58:14 +0300

    Added changes to 1.c

--- hw8/1.c
+++ hw8/1.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
-
 #define SIZE 5
 
-int creatingAnArray(int arr[]);
+int creatingAnArray(int n, int arr[]);
 
-double arithmeticMeanOfAllElements(int arr[]);
+double arithmeticMeanOfAllElements(int n, const int arr[]);
 
 int main(void)
 {
     int arr[SIZE];
-    creatingAnArray(arr);
-    printf("%.3f", arithmeticMeanOfAllElements(arr));
+    creatingAnArray(SIZE, arr);
+    printf("%.3f", arithmeticMeanOfAllElements(SIZE, arr));
     return 0;
 }
 
-int creatingAnArray(int arr[])
+int creatingAnArray(int n, int arr[])
 {
     int i;
-    for (i = 0; i < SIZE; i++)
+    for (i = 0; i < n; i++)
         scanf("%d", &arr[i]);
     return i;
 }
 
-double arithmeticMeanOfAllElements(int arr[])
+double arithmeticMeanOfAllElements(int n, const int arr[])
 {
     int sumElem = 0;
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < n; i++)
         sumElem += arr[i];
     return (double) sumElem / 5;
 }
