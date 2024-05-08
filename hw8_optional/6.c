f002ae438f655321b6629032166496abd91500ec
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-05-08 20:32:51 +0300

    Added 6.c to hw8_optional

---
+++ hw8_optional/6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define SIZE_ARR 12
+
+int creat_array(int sizeArr, int arr[]);
+
+void print_arithmetic_mean(int sizeArr, int arr[]);
+
+int main(void)
+{
+    int arr[SIZE_ARR];
+    creat_array(SIZE_ARR, arr);
+    print_arithmetic_mean(SIZE_ARR, arr);
+    return 0;
+}
+
+int creat_array(int sizeArr, int arr[])
+{
+    int i;
+    for (i = 0; i < sizeArr; i++)
+        scanf("%d", &arr[i]);
+    return i;
+}
+
+void print_arithmetic_mean(int sizeArr, int arr[])
+{
+    int sum = 0;
+    for(int i = 0; i < sizeArr; i++)
+        sum += arr[i];
+    printf("%.2f", (double) sum / sizeArr);
+}
