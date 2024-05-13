7bdf363cf90c54b2045cad008f3cf72344f4a6ba
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-05-13 19:34:09 +0300

    Added 6.c to hw9

---
+++ hw9/6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define COUNT 5
+
+int is_two_same(int size, int a[]);
+
+int main(void)
+{
+    int arr[COUNT] = {1, 1, 3, 4, 5};
+    printf("%d", is_two_same(COUNT, arr));
+    return 0;
+}
+
+int is_two_same(int size, int a[])
+{
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = i+1; j < size; j++)
+        {
+            if(a[i] == a[j])
+                return 1;
+        }
+
+    }
+    return 0;
+}
