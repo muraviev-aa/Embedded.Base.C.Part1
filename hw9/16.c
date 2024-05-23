a5418ad9aabca33abe7d908cfc3a7a7b7aefde1b
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-05-23 08:45:13 +0300

    Added changes to 16.c

--- hw9/16.c
+++ hw9/16.c
@@ -14,33 +14,5 @@ int main(void)
 
 int from_letter_number(char str[])
 {
-    int letter = 0;
-    switch (str[0])
-    {
-        case 'A':
-            letter = 1;
-            break;
-        case 'B':
-            letter = 2;
-            break;
-        case 'C':
-            letter = 3;
-            break;
-        case 'D':
-            letter = 4;
-            break;
-        case 'E':
-            letter = 5;
-            break;
-        case 'F':
-            letter = 6;
-            break;
-        case 'G':
-            letter = 7;
-            break;
-        case 'H':
-            letter = 8;
-            break;
-    }
-    return letter;
+    return str[0]-'A'+1;
 }
