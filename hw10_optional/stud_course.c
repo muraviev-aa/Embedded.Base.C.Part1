b388c19e1f3bec1bbcd258c4127c1361b5b0fe2a
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-05-27 08:08:38 +0300

    Added changes to stud_course.c

--- hw10_optional/stud_course.c
+++ hw10_optional/stud_course.c
@@ -7,7 +7,7 @@
 
 struct student
 {
-    char surname[STR_SIZE];
+    char surname[STR_SIZE]; //char *surname = "Ivan";
     char name[STR_SIZE];
     uint8_t age;
 };
@@ -46,7 +46,7 @@ int eldest(struct student course[], int number)
     int max = course[number].age;
     for (int i = 0; i < number; i++)
     {
-        if(max < course[i].age)
+        if (max < course[i].age)
             max = course[i].age;
     }
     return max;
@@ -57,20 +57,38 @@ int num_people_given_name(struct student *course, int number, char *name)
     int count = 0;
     for (int i = 0; i < number; i++)
     {
-        if(!strcmp(course[i].name, name))
+        if (!strcmp(course[i].name, name))
             count++;
+    }
+    return count;
+}
+
+int namesakes(struct student *course, int number)
+{
+    int count = 0;
+    for (int i = 0; i < number - 1; ++i)
+    {
+        for (int j = 1; j < number; ++j)
+        {
+            if(!strcmp(course[i].surname, course[j].surname))
+            {
+                count++;
+                break;
+            }
+        }
     }
     return count;
 }
 
 int main(void)
 {
-    struct student course[STUDENT_NUMBER]; // ghbyn
+    struct student course[STUDENT_NUMBER]; // массив из 200 структур
     int number = add_course(course);
     printf("%d\n", number);
     print_course(course, number);
     printf("Eldest student is %d\n", eldest(course, number));
     char *name = "Ivan";
     printf("Name %s number = %d\n", name, num_people_given_name(course, number, name));
+    printf("Same surname number = %d\n", namesakes(course, number));
     return 0;
 }
